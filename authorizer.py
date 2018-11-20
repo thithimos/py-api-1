3a1ff762ef1b77b238908cc8106a08e0c75ab6a9
mmanning <michael.manning@bcdtravel.com>
2018-11-19 19:48:24 -0500

    added jwt validation logic
    Change-Id: If3ac1d354e5d7c626bb3090c5af9e66ac4779d32

--- authorizer.py
+++ authorizer.py
@@ -1,4 +1,7 @@
 import os
+import jwt
+import urllib.request
+import json
 
 def handler(event, context):
     authorization_header_value = event['headers']['Authorization']
@@ -19,13 +22,86 @@ def handler(event, context):
         print('Invalid authorization header value')
         raise Exception('Unauthorized')
     
-    policy = generate_policy('Allow', event['methodArn'])
-    return policy
+    token = token_parts[1]
+    
+    #check the signature algo
+    with urllib.request.urlopen('https://thithimos.auth0.com/.well-known/jwks.json') as response:
+        jwks = json.loads(response.read())
+        print(f'jwks: {jwks}')
+        unverified_header = jwt.get_unverified_header(token)
+        print(f'unverified_header: {unverified_header}')
+
+    rsa_key = {}
+    public_key = ''
+    for key in jwks["keys"]:
+        if key["kid"] == unverified_header["kid"]:
+            rsa_key = {
+                "kty": key["kty"],
+                "kid": key["kid"],
+                "use": key["use"],
+                "n": key["n"],
+                "e": key["e"]
+            }
+            public_key = key['x5c']
+
+    if rsa_key:
+        try:
+            principal_id = jwt_verify(token, public_key)
+            #principal_id = jwt.decode(
+            #    token,
+            #    public_key,
+            #    algorithms=['RS256'],
+            #    audience='https://api-cancel-auth.tripsource.com',
+            #    issuer='https://thithimos.auth0.com/'
+            #)
+        except jwt.ExpiredSignatureError:
+            print('token expired')
+            raise Exception('Unauthorized')
+        except jwt.MissingRequiredClaimError:
+            print('missing required claim')
+            raise Exception('Unauthorized')
+        except jwt.DecodeError:
+            print('decode error')
+            raise Exception('Unauthorized')
+        except jwt.InvalidTokenError as e:
+            print(f'invalid token error: {e}')
+            raise Exception('Unauthorized')
+        except Exception as e:
+            print(f'invalid header - unable to parse authentication token: {e}')
+            raise Exception('Unauthorized')
+
+        policy = generate_policy(principal_id, 'Allow', event['methodArn'])
+        return policy
+        
+    print('unable to find appropriate key')
+    raise Exception('Unauthorized')
+
+    #check the permissions
+
+def jwt_verify(auth_token, public_key):
+    public_key = format_public_key(public_key)
+    pub_key = convert_certificate_to_pem(public_key)
+    payload = jwt.decode(auth_token, pub_key, algorithms=['RS256'], audience='https://api-cancel-auth.tripsource.com')
+    return payload['sub']
 
 
-def generate_policy(effect, resource):
+def format_public_key(public_key):
+    public_key = public_key.replace('\n', ' ').replace('\r', '')
+    public_key = public_key.replace('-----BEGIN CERTIFICATE-----', '-----BEGIN CERTIFICATE-----\n')
+    public_key = public_key.replace('-----END CERTIFICATE-----', '\n-----END CERTIFICATE-----')
+    return public_key
+
+
+def convert_certificate_to_pem(public_key):
+    cert_str = public_key.encode()
+    cert_obj = load_pem_x509_certificate(cert_str, default_backend())
+    pub_key = cert_obj.public_key()
+    return pub_key
+
+
+def generate_policy(principal_id, effect, resource):
     return {
-        #'principalId': principal_id,
+        'principalId': principal_id,
         'policyDocument': {
             'Version': '2012-10-17',
             'Statement': [
